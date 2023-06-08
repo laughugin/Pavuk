9d512a55832c5e4ca9c4c6fa14b653f4a42cb62f
laughugin <90358540+laughugin@users.noreply.github.com>
2023-06-08 23:37:28 +0200

    GUI

--- GUI.java
+++ GUI.java
@@ -7,7 +7,7 @@ public class GUI {
     public static int  frameWidth = 800;
     
 
-    public static void main(String[] args) {
+    public static void Frame() {
         JFrame frame = new JFrame("Frame"); // main frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(frameHeight, frameWidth);
