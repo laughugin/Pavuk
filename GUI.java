44c153d30686c4cdb91035541849fc29d984cf6d
laughugin <90358540+laughugin@users.noreply.github.com>
2023-06-08 23:36:10 +0200

    GUI

---
+++ GUI.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+
+import javax.swing.*;
+
+public class GUI {
+    public static int  frameHeight = 800; //frame setup
+    public static int  frameWidth = 800;
+    
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Frame"); // main frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(frameHeight, frameWidth);
+        frame.getContentPane().setBackground(Color.darkGray);;
+        frame.setLocationRelativeTo(null); 
+        frame.setVisible(true);
+    }
+}
