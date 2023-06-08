2118ea0ff6e70e5f616b3e0a9776fa45f63d6613
650THz <vlad.hashencko@gmail.com>
2023-06-08 23:55:54 +0200

    new changes

--- GUI.java
+++ GUI.java
@@ -2,6 +2,13 @@ import java.awt.Color;
 
 import javax.swing.*;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+
 public class GUI {
     public static int  frameHeight = 800; //frame setup
     public static int  frameWidth = 800;
@@ -15,4 +22,23 @@ public class GUI {
         frame.setLocationRelativeTo(null); 
         frame.setVisible(true);
     }
+
+    public static BufferedImage MatrixToImage(int [][] Matrix,int size_x,int size_y){
+        try {
+            BufferedImage image;
+            for(int i=0; i< size_x; i++) {
+                for(int j = 0; j < size_y; j++) {
+                    int a = Matrix[i][j];
+                    Color newColor = new Color(a,a,a);
+                    image.setRGB(j, i, newColor.getRGB());
+                }
+            }
+            File output = new File("GrayScale.jpg");
+            ImageIO.write(image, "jpg", output);
+            return image;
+        }
+        catch(Exception e) {
+            return null;
+        }
+    } 
 }
