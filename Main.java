301bd98d876737d2a435d650e266b9f983fca0b7
laughugin <90358540+laughugin@users.noreply.github.com>
2023-06-09 02:08:33 +0200

    new changes

--- Main.java
+++ Main.java
@@ -3,21 +3,34 @@ import java.awt.Point;
 
 public class Main {
     public static int[][] frameMatrix; // matrix of the frame
-    public static int numberOfPoints = 50000; // num of generated points
+    public static int numberOfPoints = 100; // num of generated points
     public static int CursorPosX = 0;
     public static int CursorPosY = 0;
-
+    private static int spiderX = 0;
+    private static int spiderY = 0;
+    private static final int SPIDER_WIDTH = 15;
+    private static final int SPIDER_HEIGHT = 11;
+    private static final int SPIDER_WIDTH2 = 9;
+    private static final int SPIDER_HEIGHT2 = 15;
+    
     public static void main(String[] args) {
         GUI gui = new GUI();
+
+
+        spiderX = gui.frameWidth/2;
+        spiderY = gui.frameHeight/2;
         frameMatrix = Matrix.Fill(0, gui.frameWidth, gui.frameHeight);
         gui.Frame(frameMatrix);
         frameMatrix = new int[gui.frameWidth][gui.frameHeight];
         PointGenerator gen1 = new PointGenerator(numberOfPoints, frameMatrix, gui.frameWidth, gui.frameHeight);
         frameMatrix = gen1.generatePoints();
-        gui.Refresh(frameMatrix);
+        
+        
 
         // Read cursor position
         while (true) {
+            fillRectangle(frameMatrix, spiderX, spiderY, SPIDER_WIDTH, SPIDER_HEIGHT);
+            fillRectangle(frameMatrix, spiderX, spiderY, SPIDER_WIDTH2, SPIDER_HEIGHT2);
             // Get the cursor position relative to the screen
             Point cursorPos = MouseInfo.getPointerInfo().getLocation();
 
@@ -37,6 +50,7 @@ public class Main {
             clearConsole();
 
             // Print the cursor position
+            
             System.out.println("Cursor:\nx = " + CursorPosX + "\ny = " + CursorPosY);
 
             try {
@@ -44,8 +58,18 @@ public class Main {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            gui.Refresh(frameMatrix);
         }
     }
+
+    public static void fillRectangle(int[][] matrix, int x, int y, int width, int height) {
+        for (int i = y - height/2; i < y + height/2; i++) {
+            for (int j = x - width/2; j < x + width/2; j++) {
+                matrix[i][j] = 255;
+            }
+        }
+    }
+
 
     public static void clearConsole() {
         try {
