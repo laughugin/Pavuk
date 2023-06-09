822c1a8900762c8bc3ad033c5140462e14539f91
laughugin <90358540+laughugin@users.noreply.github.com>
2023-06-09 03:31:00 +0200

    new changes

--- Main.java
+++ Main.java
@@ -4,33 +4,45 @@ import java.awt.Point;
 public class Main {
     public static int[][] frameMatrix; // matrix of the frame
     public static int numberOfPoints = 100; // num of generated points
-    public static int CursorPosX = 0;
+    public static int CursorPosX = 0; 
     public static int CursorPosY = 0;
     private static int spiderX = 0;
     private static int spiderY = 0;
-    private static final int SPIDER_WIDTH = 15;
+    private static final int SPIDER_WIDTH = 15; //spider consisting of two rectangles parameters
     private static final int SPIDER_HEIGHT = 11;
     private static final int SPIDER_WIDTH2 = 9;
     private static final int SPIDER_HEIGHT2 = 15;
+    public static final int POINT_COLOR = 255;
+    public static final int SPIDER_COLOR = 150;
+    public static final int PATH_COLOR = 100;
+    public static final int SPIDER_PATH_COLOR = 50;
+    
     
     public static void main(String[] args) {
-        GUI gui = new GUI();
-
-
-        spiderX = gui.frameWidth/2;
-        spiderY = gui.frameHeight/2;
-        frameMatrix = Matrix.Fill(0, gui.frameWidth, gui.frameHeight);
+        GUI gui = new GUI(); // run gui
+        spiderX = gui.frameWidth/2; // first center position of the spider
+        spiderY = gui.frameHeight/2; 
+        frameMatrix = Matrix.Fill(0, gui.frameWidth, gui.frameHeight); // fill the matrix
         gui.Frame(frameMatrix);
         frameMatrix = new int[gui.frameWidth][gui.frameHeight];
-        PointGenerator gen1 = new PointGenerator(numberOfPoints, frameMatrix, gui.frameWidth, gui.frameHeight);
+        PointGenerator gen1 = new PointGenerator(numberOfPoints, frameMatrix, gui.frameWidth, gui.frameHeight); // generate inital points 
         frameMatrix = gen1.generatePoints();
         
         
 
         // Read cursor position
         while (true) {
-            fillRectangle(frameMatrix, spiderX, spiderY, SPIDER_WIDTH, SPIDER_HEIGHT);
+
+            fillRectangle(frameMatrix, spiderX, spiderY, SPIDER_WIDTH, SPIDER_HEIGHT); // fill the spider on the frame
             fillRectangle(frameMatrix, spiderX, spiderY, SPIDER_WIDTH2, SPIDER_HEIGHT2);
+            for (int i = 0; i < gui.frameWidth; i++){
+                for (int j = 0; j < gui.frameHeight; j++){
+                    if(frameMatrix[i][j] == POINT_COLOR){  // if the cell is 255 (so it's white meaning belongs to spider or point) -> fill the line
+                        fillBetweenPoints(frameMatrix, spiderX, spiderY, i, j);
+                    }
+                }
+
+            }
             // Get the cursor position relative to the screen
             Point cursorPos = MouseInfo.getPointerInfo().getLocation();
 
@@ -58,18 +70,93 @@ public class Main {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            int dx = CursorPosX - spiderX;
+            int dy = CursorPosY - spiderY;
+                
+            // Define the speed at which the spider moves
+            int speed = 5; // Adjust as needed
+                
+            // Calculate the total distance
+            double distance = Math.sqrt(dx * dx + dy * dy);
+                
+            // Calculate the normalized direction vector
+            double directionX = dx / distance;
+            double directionY = dy / distance;
+                
+            // Calculate the movement increments
+            int incrementX = (int) Math.round(directionX * speed);
+            int incrementY = (int) Math.round(directionY * speed);
+                
+            // Delete the previous path with color value of 100
+            //deletePreviousPath(frameMatrix);
+                
+            // Update the spider position with the increments
+            spiderX += incrementX;
+            spiderY += incrementY;
+
+
             gui.Refresh(frameMatrix);
+            deletePreviousPath(frameMatrix);
+            deletePreviousSpider(frameMatrix);
         }
     }
 
+    static void fillBetweenPoints(int[][] matrix, int startX, int startY, int endX, int endY) { //!!!!!!!!! make it work
+        int dx = Math.abs(endX - startX);
+        int dy = Math.abs(endY - startY);
+        int sx = startX < endX ? 1 : -1;
+        int sy = startY < endY ? 1 : -1;
+        int err = dx - dy;
+        
+        while (startX != endX || startY != endY) {
+            if (matrix[startY][startX] != POINT_COLOR) {
+                matrix[startY][startX] = PATH_COLOR;
+            }
+        
+            int err2 = 2 * err;
+        
+            if (err2 > -dy) {
+                err -= dy;
+                startX += sx;
+            } else if (err2 < dx) {
+                err += dx;
+                startY += sy;
+            }
+        }
+    
+        matrix[endY][endX] = PATH_COLOR;
+    }
+
+    
     public static void fillRectangle(int[][] matrix, int x, int y, int width, int height) {
         for (int i = y - height/2; i < y + height/2; i++) {
             for (int j = x - width/2; j < x + width/2; j++) {
-                matrix[i][j] = 255;
+                matrix[i][j] = SPIDER_COLOR;
+            }
+        }
+    }
+    
+    private static void deletePreviousPath(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == PATH_COLOR) {
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
 
+    private static void deletePreviousSpider(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == SPIDER_COLOR) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+    
 
     public static void clearConsole() {
         try {
